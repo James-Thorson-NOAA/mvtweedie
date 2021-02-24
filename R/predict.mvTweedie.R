e77f11f7b8f19f4eb2b462451e6d86ce476abe51
Jim Thorson <50178738+James-Thorson-NOAA@users.noreply.github.com>
2021-02-23 19:12:40 -0800

    update docs

--- R/predict.mvTweedie.R
+++ R/predict.mvTweedie.R
@@ -8,32 +8,44 @@
 #' model is interpreted as a multivariate logit Tweedie distribution. This function
 #' does this transformation for a model fitted using:
 #' \itemize{
-#' \item A gam using \code{\link[mgcv]{gam}}
-#' \item A GLMM using \code{\link[glmmTMB]{glmmTMB}}
+#' \item A generalized additive model (GAM) using \code{\link[mgcv]{gam}}
+#' \item A generalized linear mixed model (GLMM) using \code{\link[glmmTMB]{glmmTMB}}
 #' }
+#' It then also calculates an approximation to the standard error for this proportion
+#'
+#' @inheritParams stats::predict.lm
+#' @param x output from \code{\link[mgcv]{gam}} or \code{\link[glmmTMB]{glmmTMB}}, but with
+#'    \code{class(x)=c("mvTweedie",...)} where \code{...} indicates the original values for
+#'    \code{class(x)}
+#' @param category_name name of column that indicates grouping variable
+#' @param origdata original data used when fitting
+#'
 #'
 #' @method predict mvTweedie
 #' @export
 predict.mvTweedie <-
 function(x,
 #                  original_class = "glmmTMB",
+                  category_name = "group",
                   newdata,
                   origdata = x$frame,
                   se.fit = FALSE )
 {
   # Error checks
+  if( tolower(substr(family(x)$family,1,7)) != "tweedie" ) error("`predict.mvTweedie` only implemented for a Tweedie distribution")
+  if( family(x)$link != "log" ) error("`predict.mvTweedie` only implemented for a log link")
   if( se.fit==TRUE & "fit_model"%in%class(x) ) error("se.fit not implemented for predict using VAST")
 
   # Defaults
   if(missing(newdata) || is.null(newdata)) newdata = origdata
 
   # Predict each observation for each class
-  se_pred_ic = pred_ic = array(NA, dim=c(nrow(newdata),nlevels(origdata[,'group'])))
+  se_pred_ic = pred_ic = array(NA, dim=c(nrow(newdata),nlevels(origdata[,category_name])))
   for(cI in 1:ncol(pred_ic)){
 
     # Modify data
     data = newdata
-    data[,'group'] = factor( levels(origdata[,'group'])[cI], levels=levels(origdata[,'group']) )
+    data[,category_name] = factor( levels(origdata[,category_name])[cI], levels=levels(origdata[,category_name]) )
 
     # Modify class
     class(x) = setdiff( class(x), "mvTweedie" )
@@ -67,14 +79,14 @@ function(x,
   # Normalize probability for each observation and class
   rowsum_pred_ic = outer( rowSums(pred_ic), rep(1,ncol(pred_ic)) )
   prob_ic = pred_ic / rowsum_pred_ic
-  prob_i = prob_ic[ cbind(1:nrow(pred_ic), match(newdata[,'group'],levels(origdata[,'group']))) ]
+  prob_i = prob_ic[ cbind(1:nrow(pred_ic), match(newdata[,category_name],levels(origdata[,category_name]))) ]
 
   # return prediction
   if( se.fit==TRUE ){
     # Normalize SE-squared for each observation and class
     rowsum_se2_ic = outer( rowSums(se_pred_ic^2), rep(1,ncol(pred_ic)) )
     se2_prob_ic = prob_ic^2 * ( se_pred_ic^2/pred_ic^2 - 2*se_pred_ic^2/(pred_ic*rowsum_pred_ic) + rowsum_se2_ic/rowsum_pred_ic^2 )
-    se_i = sqrt(se2_prob_ic[ cbind(1:nrow(se2_prob_ic), match(newdata[,'group'],levels(origdata[,'group']))) ])
+    se_i = sqrt(se2_prob_ic[ cbind(1:nrow(se2_prob_ic), match(newdata[,category_name],levels(origdata[,category_name]))) ])
     out = list("fit"=prob_i, "se.fit"=se_i)
   }else{
     out = prob_i
